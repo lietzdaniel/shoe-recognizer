3b144a5d2bd9e2a4bb1b6327a17ce1bd827f1612
Daniel Lietz <ge96jek@mytum.de>
2023-05-10 23:47:00 +0200

    updated duckScrape to properly implement the behaviour stated in the coming readme.md

--- ImageScraping/duckScrape.py
+++ ImageScraping/duckScrape.py
@@ -2,16 +2,25 @@ from jmd_imagescraper.core import *
 from pathlib import Path
 import csv
 
-filename = 'allShoes.csv'
-filename = 'popularShoes.csv'
+allFile= 'allShoes.csv'
+popularFile = 'popularShoes.csv'
 
-with open(filename, 'r') as csvfile:
+with open(popularFile, 'r') as csvfile:
     datareader = csv.reader(csvfile)
     for row in datareader:
         root = Path().cwd()/"images"
-        duckduckgo_search(Path().cwd()/row[0],keywords= row[0] ,label="all" , max_results=300)
-        duckduckgo_search(Path().cwd()/row[0],keywords= row[0] + " ebay",label="all" , max_results=300)
-        duckduckgo_search(Path().cwd()/row[0],keywords= row[0] + " vinted",label="all" , max_results=300)
-        duckduckgo_search(Path().cwd()/row[0],keywords= row[0] + " used",label="all" , max_results=300)
+        duckduckgo_search(Path().cwd()/row[0],keywords= row[0] ,label="all" , max_results=100)
+        duckduckgo_search(Path().cwd()/row[0],keywords= row[0] + " ebay",label="all" , max_results=100)
+        duckduckgo_search(Path().cwd()/row[0],keywords= row[0] + " vinted",label="all" , max_results=100)
+        duckduckgo_search(Path().cwd()/row[0],keywords= row[0] + " used",label="all" , max_results=100)
+        
+with open(allFile, 'r') as csvfile:
+    datareader = csv.reader(csvfile)
+    for row in datareader:
+        root = Path().cwd()/"images"
+        duckduckgo_search(Path().cwd()/row[0],keywords= row[0] ,label="all" , max_results=20)
+        duckduckgo_search(Path().cwd()/row[0],keywords= row[0] + " ebay",label="all" , max_results=20)
+        duckduckgo_search(Path().cwd()/row[0],keywords= row[0] + " vinted",label="all" , max_results=20)
+        duckduckgo_search(Path().cwd()/row[0],keywords= row[0] + " used",label="all" , max_results=20)
         
     
