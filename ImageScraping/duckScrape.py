2807eb5065347a625500dfe4ead95cd6a3895fd0
Daniel Lietz <ge96jek@mytum.de>
2023-05-11 00:36:16 +0200

    updated so it doesnt do popular shoes twice

--- ImageScraping/duckScrape.py
+++ ImageScraping/duckScrape.py
@@ -4,10 +4,11 @@ import csv
 
 allFile= 'allShoes.csv'
 popularFile = 'popularShoes.csv'
-
+popular = []
 with open(popularFile, 'r') as csvfile:
     datareader = csv.reader(csvfile)
     for row in datareader:
+        popular.append(row[0])
         root = Path().cwd()/"images"
         duckduckgo_search(Path().cwd()/row[0],keywords= row[0] ,label="all" , max_results=100)
         duckduckgo_search(Path().cwd()/row[0],keywords= row[0] + " ebay",label="all" , max_results=100)
@@ -17,6 +18,8 @@ with open(popularFile, 'r') as csvfile:
 with open(allFile, 'r') as csvfile:
     datareader = csv.reader(csvfile)
     for row in datareader:
+        if row[0] in popular:
+            continue
         root = Path().cwd()/"images"
         duckduckgo_search(Path().cwd()/row[0],keywords= row[0] ,label="all" , max_results=20)
         duckduckgo_search(Path().cwd()/row[0],keywords= row[0] + " ebay",label="all" , max_results=20)
